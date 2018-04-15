d7fd4a8cad86139770b2df16c45ee316fb29c9cb
tanzeelak <tnkhan8042@gmail.com>
2018-04-14 21:41:42 -0700

    first

---
+++ hw2/hw2.ml
@@ -0,0 +1,63 @@
+type ('nonterminal, 'terminal) symbol =
+| N of 'nonterminal
+| T of 'terminal
+;;
+
+type awksub_nonterminals =
+| Expr | Lvalue | Incrop | Binop | Num
+;;
+
+let awksub_rules =
+   [Expr, [T"("; N Expr; T")"];
+    Expr, [N Num];
+    Expr, [N Expr; N Binop; N Expr];
+    Expr, [N Lvalue];
+    Expr, [N Incrop; N Lvalue];
+    Expr, [N Lvalue; N Incrop];
+    Lvalue, [T"$"; N Expr];
+    Incrop, [T"++"];
+    Incrop, [T"--"];
+    Binop, [T"+"];
+    Binop, [T"-"];
+    Num, [T"0"];
+    Num, [T"1"];
+    Num, [T"2"];
+    Num, [T"3"];
+    Num, [T"4"];
+    Num, [T"5"];
+    Num, [T"6"];
+    Num, [T"7"];
+    Num, [T"8"];
+    Num, [T"9"]]
+;;
+
+let rec convert_rules nt rules =
+match rules with
+| [] -> []
+| currRule :: t ->
+if nt = (fst currRule) then
+(snd currRule) :: convert_rules nt t
+else
+convert_rules nt t
+;;
+
+let convert_grammar g =
+(fst g, (fun nt -> convert_rules nt (snd g)))
+;;
+
+
+convert_grammar (Expr,
+      [Expr, [N Num];
+       Expr, [N Lvalue];
+       Expr, [N Expr; N Lvalue];
+       Expr, [N Lvalue; N Expr];
+       Expr, [N Expr; N Binop; N Expr];
+       Lvalue, [N Lvalue; N Expr];
+       Lvalue, [N Expr; N Lvalue];
+       Lvalue, [N Incrop; N Lvalue];
+       Lvalue, [N Lvalue; N Incrop];
+       Incrop, [T"++"]; Incrop, [T"--"];
+       Binop, [T"+"]; Binop, [T"-"];
+       Num, [T"0"]; Num, [T"1"]; Num, [T"2"]; Num, [T"3"]; Num, [T"4"];
+       Num, [T"5"]; Num, [T"6"]; Num, [T"7"]; Num, [T"8"]; Num, [T"9"]])
+;;
