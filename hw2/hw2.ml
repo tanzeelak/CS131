82801f45113026c3cf9a361a997961c9718b7bdf
tanzeelak <tnkhan8042@gmail.com>
2018-04-18 11:13:26 -0700

    with prod func but wtf

--- hw2/hw2.ml
+++ hw2/hw2.ml
@@ -4,7 +4,7 @@ type ('nonterminal, 'terminal) symbol =
 ;;
 
 type awksub_nonterminals =
-| Expr | Lvalue | Incrop | Binop | Num
+| Expr | Term | Lvalue | Incrop | Binop | Num
 ;;
 
 let awksub_rules =
@@ -31,20 +31,71 @@ let awksub_rules =
     Num, [T"9"]]
 ;;
 
-let rec convert_rules nt rules =
+let rec match_rules nonterminal rules =
 match rules with
 | [] -> []
-| currRule :: t ->
-if nt = (fst currRule) then
-(snd currRule) :: convert_rules nt t
+| headRule :: restRules ->
+let lhsRule = fst headRule in
+let rhsRule = snd headRule in
+if nonterminal = (lhsRule) then
+(rhsRule) :: match_rules nonterminal restRules
 else
-convert_rules nt t
+match_rules nonterminal restRules
 ;;
 
 let convert_grammar g =
-(fst g, (fun nt -> convert_rules nt (snd g)))
+let startExp = fst g in
+let rules = snd g in
+(startExp, (fun nonterminal -> match_rules nonterminal rules))
 ;;
 
+
+
+let is_terminal_rule rule = 
+  match rule with
+| T x -> true
+  | N x -> false
+;;
+
+
+(*
+
+
+let rec check_symbols gram rules acceptor deriv frag = 
+match rules with
+| [] -> acceptor deriv frag
+| (N headRule::tailRules) ->
+//call matcher
+| (T headRule::tailRules) ->
+  match frag with
+  | [] -> None
+  | headFrag::tailFrag ->
+    if headFrag = headRule then
+    check_symbols gram rules acceptor deriv tailFrag
+    else None
+
+//Takes rhs [T "3"; N Num] and calls check_symbols on them
+let check_rhs rules start acceptor deriv frag = 
+
+
+
+
+let parse_prefix gram frag accept =
+match gram with
+| (startExp, rules) ->
+check_rhs rules startExp acceptor [] frag
+
+
+| //apply matcher to accept and frag, matcher must return first acceptable match of a prefix of frag
+//by trying grammar rules in order
+//match succeed if e
+if acceptor != None then
+acceptor
+else
+None
+
+
+*)
 
 convert_grammar (Expr,
       [Expr, [N Num];
@@ -61,3 +112,33 @@ convert_grammar (Expr,
        Num, [T"0"]; Num, [T"1"]; Num, [T"2"]; Num, [T"3"]; Num, [T"4"];
        Num, [T"5"]; Num, [T"6"]; Num, [T"7"]; Num, [T"8"]; Num, [T"9"]])
 ;;
+
+
+let awkish_grammar =
+  (Expr,
+   function
+     | Expr ->
+         [[N Term; N Binop; N Expr];
+          [N Term]]
+	  | Term ->
+ [[N Num];
+  [N Lvalue];
+  [N Incrop; N Lvalue];
+  [N Lvalue; N Incrop];
+  [T"("; N Expr; T")"]]
+     | Lvalue ->
+      [[T"$"; N Expr]]
+      | Incrop ->
+ [[T"++"];
+  [T"--"]]
+     | Binop ->
+      [[T"+"];
+        [T"-"]]
+	| Num ->
+ [[T"0"]; [T"1"]; [T"2"]; [T"3"]; [T"4"];
+  [T"5"]; [T"6"]; [T"7"]; [T"8"]; [T"9"]])
+;;
+
+
+(* prodduc func *) 
+(snd awkish_grammar) Expr;;
