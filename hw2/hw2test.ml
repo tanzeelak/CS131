7b2b028e4a935ee19b8441aa527d12881cad3507
tanzeelak <tnkhan8042@gmail.com>
2018-04-21 15:29:07 -0700

    actually

--- hw2/hw2test.ml
+++ hw2/hw2test.ml
@@ -3,6 +3,23 @@ let accept_empty_suffix derivation = function
    | [] -> Some (derivation, [])
    | _ -> None
 
+
+(*
+ let includes_zero derivation = function
+ | [] -> false
+| (_,"0")::_ -> true
+| h::t -> includes t
+;;
+
+let accept_includes_zero derivation string =
+if includes_zero derivation then
+Some(derivation, string)
+else
+None
+;;
+*)
+
+
 let g1 = 
 (Expr,
  function
@@ -31,22 +48,16 @@ let g1 =
      [T"5"]; [T"6"]; [T"7"]; [T"8"]; [T"9"]])
 ;;
 
-     parse_prefix g1 accept_all ["4";"%";"/";"8";"-";"("; "$"; "8"; "++"; ")"];;
-(*
-     let test_1 =
-     ((parse_prefix g1 accept_all ["4"; "%"; "/"; "1"; "+"])
-		    = Some
-		    ([
-		     (Expr, [N Term; N Binop; N Expr]);
-			    (Term, [N Num]);
-			    (Num, [T "4"]);
-			    (Binop, [T "/"]);
-			    (Expr, [N Term]);
-			    (Term, [N Lvalue]);
-			    (Lvalue, [T "$"; N Expr]);
-				     (Expr, [N Term]);
-				     (Term, [N Num]);
-				      (Num, [T "1"])],
-				      ["+"]
-				      ))
-*)
+let test_1 = (
+parse_prefix g1 accept_all ["4";"%";"/";"8";"-";"("; "$"; "8"; "++"; ")"]
+= 
+Some
+ ([(Expr, [N Term; N Binop; N Expr]); (Term, [N Num; T "%"]); (Num, [T "4"]);
+   (Binop, [T "/"]); (Expr, [N Term; N Binop; N Expr]); (Term, [N Num]);
+   (Num, [T "8"]); (Binop, [T "-"]); (Expr, [N Term]);
+   (Term, [T "("; N Expr; T ")"]); (Expr, [N Term]);
+   (Term, [N Lvalue; N Incrop]); (Lvalue, [T "$"; N Expr]); (Expr, [N Term]);
+   (Term, [N Num]); (Num, [T "8"]); (Incrop, [T "++"])],
+  [])
+)
+;;
