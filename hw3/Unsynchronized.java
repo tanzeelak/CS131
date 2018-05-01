2564c75a6887445ef7ac4b8e3149c4aa399cb7eb
tanzeelak <tnkhan8042@gmail.com>
2018-04-30 20:24:28 -0700

    in the jar file and unsync

--- hw3/Unsynchronized.java
+++ hw3/Unsynchronized.java
@@ -1,16 +1,17 @@
-class SynchronizedState implements State {
+
+class UnsynchronizedState implements State {
     private byte[] value;
     private byte maxval;
 
-    SynchronizedState(byte[] v) { value = v; maxval = 127; }
+    UnsynchronizedState(byte[] v) { value = v; maxval = 127; }
 
-    SynchronizedState(byte[] v, byte m) { value = v; maxval = m; }
+    UnsynchronizedState(byte[] v, byte m) { value = v; maxval = m; }
 
     public int size() { return value.length; }
 
     public byte[] current() { return value; }
 
-    public synchronized boolean swap(int i, int j) {
+    public boolean swap(int i, int j) {
 	if (value[i] <= 0 || value[j] >= maxval) {
 	    return false;
 	}
