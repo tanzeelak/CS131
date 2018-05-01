0d8ebdc9a300984d0abe0e3b2284c5e5c13a601c
tanzeelak <tnkhan8042@gmail.com>
2018-05-01 15:59:11 -0700

    up to getnset

---
+++ hw3/GetNSet.java
@@ -0,0 +1,53 @@
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+class GetNSet implements State {
+    private byte[] value;
+    private AtomicIntegerArray valueAtomic;
+    private byte maxval;
+
+    private AtomicIntegerArray copyToAtomicArray(byte[] v) {
+	int vSize = v.length;
+	int[] array = new int[vSize];
+	for (int i = 0; i < vSize; i++){
+	    array[i] = v[i];
+	}
+	valueAtomic = new AtomicIntegerArray(array);
+	return valueAtomic;
+    }
+
+    private byte[] copyToByteArray(){
+	int aSize = valueAtomic.length();
+	byte[] array = new byte[aSize];
+	for (int i = 0; i < aSize; i++) {
+	    array[i] = (byte) valueAtomic.get(i);
+	}
+	return array;
+    }
+    
+    GetNSet(byte[] v) {
+	copyToAtomicArray(v);
+	maxval = 127;
+    }
+
+    GetNSet(byte[] v, byte m) {
+	copyToAtomicArray(v);
+	maxval = m;
+    }
+
+    public int size() {
+	return valueAtomic.length();
+    }
+
+    public byte[] current() {
+	return copyToByteArray();
+    }
+
+    public boolean swap(int i, int j) {
+	if (valueAtomic.get(i) <= 0 || valueAtomic.get(j) >= maxval) {
+	    return false;
+	}
+	valueAtomic.decrementAndGet(i);
+	valueAtomic.incrementAndGet(j);
+	return true;
+    }
+}
--- hw3/UnsafeMemory.java
+++ hw3/UnsafeMemory.java
@@ -16,6 +16,10 @@ class UnsafeMemory {
 		s = new NullState(stateArg, maxval);
 	    else if (args[0].equals("Synchronized"))
 		s = new SynchronizedState(stateArg, maxval);
+	    else if (args[0].equals("Unsynchronized"))
+                s = new UnsynchronizedState(stateArg, maxval);
+            else if (args[0].equals("GetNSet"))
+                s = new GetNSet(stateArg, maxval);
 	    else
 		throw new Exception(args[0]);
 	    dowork(nThreads, nTransitions, s);
