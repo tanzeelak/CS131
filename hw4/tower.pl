6259f724b2aa82877f821dcd1142357ec0517c5a
tanzeelak <tnkhan8042@gmail.com>
2018-05-15 11:58:35 -0700

    tower eval to true for one input

--- hw4/tower.pl
+++ hw4/tower.pl
@@ -13,9 +13,10 @@ isMatrix(M, Row, Col) :-
 isMatrixValid(M, RowCnt, ColCnt, N) :-
     length(M, RowCnt), /* length(?List,?Int): counts num of rows */
     maplist(rowLength(ColCnt), M), /* currying of matrix of lists: counts num of cols */
-    RowCnt=N,
-    ColCnt=N,
-    fd_domain(RowCnt, 0, N).
+    RowCnt#=N,
+    ColCnt#=N,
+    /* needs to check rows not counts */
+    fd_domain(RowCnt, 0, N). 
 
 
 accessMatrixIndex(M, I, J, Val) :-
@@ -26,6 +27,39 @@ matrixHasNLists(T, N) :-
     findall(Val, accessMatrixIndex(T, _, _, Val), Row),
     write(Row).
 
-/*topCount(M, Top) :-
+
+/*
+checkRow(N,N,0,[_]).
+checkRow(MaxHeight,N,TowerCnt,[RowHead|RowTail]) :-
+    RowHead #> #MaxHeight,
+    checkRow(RowHead,TowerCnt-1,[RowTail]).
+checkRow(MaxHeight,N,TowerCnt,[RowHead|RowTail]) :-
+    RowHead #=< #MaxHeight,
+    checkRow(MaxHeight,N,TowerCnt,[RowTail]).
+
+countLeft([MHead:MTail], N, [LeftHead:LeftTail]) :-
+    checkRow(0,N,LeftHead,MHead),
+    countLeft(MTail,N,LeftTail).
 */
-    
+
+list_empty([], true).
+list_empty([_|_], false).
+
+checkRow(_,0,[]).
+checkRow(_,0,[_]).
+checkRow(MaxHeight,TowerCnt,[RowHead|RowTail]) :-
+    TowerCnt > 0,
+    list_empty([RowHead|RowTail],false),
+    RowHead #># MaxHeight,
+    NewMaxHeight is RowHead,
+    NewTowerCnt is TowerCnt-1,
+    checkRow(NewMaxHeight,NewTowerCnt,RowTail).
+checkRow(MaxHeight,TowerCnt,[RowHead|RowTail]) :-
+    TowerCnt > 0,
+    list_empty([RowHead|RowTail],false),
+    RowHead #=<# MaxHeight,
+    checkRow(MaxHeight,TowerCnt,RowTail).
+
+countLeft([MHead:MTail],[LeftHead:LeftTail]) :-
+    checkRow(0,LeftHead,MHead),
+    countLeft(MTail,LeftTail).
