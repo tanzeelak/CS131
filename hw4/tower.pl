f792f29212d2ef1360cb409d758d688a60aa9cbb
tanzeelak <tnkhan8042@gmail.com>
2018-05-15 21:58:47 -0700

    increment CurrCnt instead of Tower

--- hw4/tower.pl
+++ hw4/tower.pl
@@ -1,7 +1,20 @@
 tower(0, [], counts([],[],[],[])).
 tower(1,[[1]], counts([1],[1],[1],[1])).
 tower(N,T,counts(Top, Bottom, Left, Right)) :-
-    isMatrixValid(T, N).
+    isMatrixValid(T, N),
+    countSide(T,Left),
+    rotate90(T, TRotate),
+    countSide(TRotate, Bottom),
+    rotate90(TRotate, TRotate2),
+    countSide(TRotate2, Right),
+    rotate90(TRotate2, TRotate3),
+    countSide(TRotate3, Top)
+.
+
+%Rotate Clauz
+rotate90(M, MRotate) :-
+    transpose(M, MTranspose),
+    maplist(reverse,MTranspose,MRotate).
 
 %Transpose Clauz
 transpose([], []).
@@ -16,7 +29,14 @@ lists_firsts_rests([], [], []).
 lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
             lists_firsts_rests(Rest, Fs, Oss).
 
-%Reverse Clauz
+%Reverz Clauz ;PPPPPPP
+
+reverseMatrix([],RevMatrix).
+reverseMatrix([MHead|MTail],RevMatrix) :-
+    reverse(MHead, RevHead),
+    append([RevHead],RevMatrix,Res),
+    reverseMatrix(MTail,Res).
+
 myReverse([],[]) :- !.
 myReverse([H|T],X) :-
     !,
@@ -48,20 +68,19 @@ isMatrixValid(M,N) :-
 list_empty([], true).
 list_empty([_|_], false).
 
-checkRow(_,0,[]).
-checkRow(MaxHeight,TowerCnt,[RowHead|RowTail]) :-
-    TowerCnt #>= 0,
+checkRow(CurrCnt,_,TowerCnt,[]) :-
+    TowerCnt is CurrCnt.
+checkRow(CurrCnt,MaxHeight,TowerCnt,[RowHead|RowTail]) :-
     list_empty([RowHead|RowTail],false),
     RowHead #> MaxHeight,
-    NewTowerCnt is TowerCnt-1,
-    checkRow(RowHead,NewTowerCnt,RowTail).
-checkRow(MaxHeight,TowerCnt,[RowHead|RowTail]) :-
-    TowerCnt #>= 0,
+    NewCurrCnt is CurrCnt+1,
+    checkRow(NewCurrCnt,RowHead,TowerCnt,RowTail).
+checkRow(CurrCnt,MaxHeight,TowerCnt,[RowHead|RowTail]) :-
     list_empty([RowHead|RowTail],false),
     RowHead #=< MaxHeight,
-    checkRow(MaxHeight,TowerCnt,RowTail).
+    checkRow(CurrCnt,MaxHeight,TowerCnt,RowTail).
 
-countSide([],[]).
-countSide([MHead|MTail],[LeftHead|LeftTail]) :-
-    checkRow(0,LeftHead,MHead),
-    countLeft(MTail,LeftTail).
+countSide(_,[],[]).
+countSide([MHead|MTail],[SideHead|SideTail]) :-
+    checkRow(0,0,SideHead,MHead),
+    countSide(MTail,SideTail).
