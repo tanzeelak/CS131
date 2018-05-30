cc7286ff0a2407fe8681374e2d689e83ceadf173
tanzeelak <tnkhan8042@gmail.com>
2018-05-29 22:20:05 -0700

    echo code

--- project/disc/echo_server.py
+++ project/disc/echo_server.py
@@ -1,10 +1,12 @@
 import asyncio
+import sys
+import argparse
 
 class EchoServerClientProtocol(asyncio.Protocol):
     def connection_made(self, transport):
         self.transport = transport
         self.peername = transport.get_extra_info('peername')
-        
+
         print('Connection from {}'.format(self.peername))
 
     def data_received(self, data):
@@ -18,25 +20,29 @@ class EchoServerClientProtocol(asyncio.Protocol):
         print('Lost connection of {}'.format(self.peername))
         self.transport.close()
 
-loop = asyncio.get_event_loop()
-# Each client connection will create a new protocol instance
+def main():
+    loop = asyncio.get_event_loop()
+    # Each client connection will create a new protocol instance
 
-# The IP address or hostname can be used.
-# 127.0.0.1 is intended for the 'localhost' loopback devices.
-# If you have multiple NIC(Network Interface Card)s, you may specify the specific IP address to be used (listen).
-# 0.0.0.0 is to use any available NIC device.
+    # The IP address or hostname can be used.
+    # 127.0.0.1 is intended for the 'localhost' loopback devices.
+    # If you have multiple NIC(Network Interface Card)s, you may specify the specific IP address to be used (listen).
+    # 0.0.0.0 is to use any available NIC device.
 
-coro = loop.create_server(EchoServerClientProtocol, '0.0.0.0', 8888)
-server = loop.run_until_complete(coro)
+    coro = loop.create_server(EchoServerClientProtocol, '0.0.0.0', 8888)
+    server = loop.run_until_complete(coro)
 
-# Serve requests until Ctrl+C is pressed
-print('Serving on {}'.format(server.sockets[0].getsockname()))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    # Serve requests until Ctrl+C is pressed
+    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
-# Close the server
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+    # Close the server
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+if __name__ == "__main__":
+    main()
---
+++ project/server.py
@@ -0,0 +1,56 @@
+import asyncio
+import aiohttp
+import sys
+import logging
+import argparse
+
+class EchoServerClientProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+        self.peername = transport.get_extra_info('peername')
+        
+        print('Connection from {}'.format(self.peername))
+
+    def data_received(self, data):
+        message = data.decode()
+        print('Data received: {!r}'.format(message))
+
+        print('Send: {!r}'.format(message))
+        self.transport.write(data)
+
+    def connection_lost(self, exc):
+        print('Lost connection of {}'.format(self.peername))
+        self.transport.close()
+
+def main():
+    loop = asyncio.get_event_loop()
+    # Each client connection will create a new protocol instance
+
+    # The IP address or hostname can be used.
+    # 127.0.0.1 is intended for the 'localhost' loopback devices.
+    # If you have multiple NIC(Network Interface Card)s, you may specify the specific IP address to be used (listen).
+    # 0.0.0.0 is to use any available NIC device.
+
+    coro = loop.create_server(EchoServerClientProtocol, '0.0.0.0', 8888)
+    server = loop.run_until_complete(coro)
+
+    # Serve requests until Ctrl+C is pressed
+    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    # Close the server
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+if __name__ == "__main__":
+    # if (len(sys.argv) != 2):
+    #     sys.stderr.write("hehe")
+    #     exit(1)
+    
+    print('Please provide an appropriate server name.')
+    exit(1)
+    main()
